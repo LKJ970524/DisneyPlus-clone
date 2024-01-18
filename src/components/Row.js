cc7e4e59ee0baf3259abc596ab7ba436bc407f74
LKJ970524 <dlrbwjd7096@naver.com>
2024-01-18 21:55:59 +0900

    영화 상세modal 창 마크업 완료

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from '../api/axios'
 import React, { useCallback, useEffect, useState } from 'react'
 import './Row.css'
+import MovieModal from './MovieModal/modal'
 
 const Row = ({title, id, fetchUrl}) => {
   const [movies, setMovies] = useState([])
+  const [modalOpen, setModalOpen] = useState(false)
+  const [movieSelected, setMovieSelected] = useState({})
 
   const fetchMovieData = useCallback( async () => {
     const response = await axios.get(fetchUrl);
@@ -14,6 +17,11 @@ const Row = ({title, id, fetchUrl}) => {
   useEffect(() => {
     fetchMovieData()
   }, [fetchMovieData])
+
+  const handleClick = (movie) => {
+    setModalOpen(true)
+    setMovieSelected(movie)
+  }
 
   return (
     <div>
@@ -35,7 +43,7 @@ const Row = ({title, id, fetchUrl}) => {
               className='row__poster'
               src= {`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
-              // onClick={() => handleClick(movie)}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -49,6 +57,13 @@ const Row = ({title, id, fetchUrl}) => {
             {'>'}</span>
         </div>
       </div>
+
+      {setModalOpen &&(
+        <MovieModal 
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
+      )}
     </div>
   )
 }
